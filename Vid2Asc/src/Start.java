897fde2d01aec3b32910f42a619786615b29c67d
Beni <benireydman901@hotmail.com>
2019-11-30 16:23:35 -0600

    Video Converter Added

--- Vid2Asc/src/Start.java
+++ Vid2Asc/src/Start.java
@@ -1,12 +1,26 @@
+import java.awt.image.BufferedImage;
 import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
+import org.bytedeco.javacv.FFmpegFrameGrabber;
+import org.bytedeco.javacv.Frame;
+import org.bytedeco.javacv.Java2DFrameConverter;
+import org.bytedeco.javacv.FrameGrabber.Exception;
 
 public class Start 
 {
-	ArrayList<File> files = new ArrayList<File>();
+	static ArrayList<File> files = new ArrayList<File>();
+	static ArrayList<BufferedImage> images = new ArrayList<BufferedImage>();
 	
 	public static void main(String[] args)
 	{
+		try {
+			videoToImages();
+		} catch (IOException e) {
+			e.printStackTrace();
+
+		}
+		
 		int cores = Runtime.getRuntime().availableProcessors();
 		for (int x = 0; x < cores; x++)
 		{
@@ -15,15 +29,22 @@ public class Start
 		}
 	}
 	
-	public void videoToImages()
+	public static void videoToImages() throws Exception
 	{
-		while()
+		FFmpegFrameGrabber g = new FFmpegFrameGrabber("C:\\Users\\Beni\\workspace\\Vid2Asc\\src\\Objects\\video.mp4");
+		g.setFormat("");
+		g.start();
+		while(true)
 		{
-			Java2DFrameConverter c = new Java2DFrameConverter;
-			c.convert(g.grab());
-			if(c == null)
+			Java2DFrameConverter c = new Java2DFrameConverter();
+			Frame currFrame = g.grab();
+			if(currFrame == null)
 				break;
+			BufferedImage curr = c.convert(currFrame);
+			images.add(curr);
 		}
+		g.stop();
+		g.close();
 	}
 
 }
